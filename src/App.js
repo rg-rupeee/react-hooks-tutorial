7a15e5252d7609cb05723f5dfeb73ada55deb89f
rg-rupeee <rupeshgarhwal3920@gmail.com>
2023-06-14 23:40:25 +0530

    useTransition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-import CounterOne from './components/customHooks/Counter/CounterOne';
-import CounterTwo from './components/customHooks/Counter/CounterTwo';
-import DocTitleOne from './components/customHooks/DocTitle/DocTitleOne';
-import DocTitleTwo from './components/customHooks/DocTitle/DocTitleTwo';
-import UserForm from './components/customHooks/userForm/UserForm';
-import UserFormHook from './components/customHooks/userForm/UserFormHook';
+import TransitionComp from './components/useTransition/TransitionComp';
 
 function App() {
   return (
     <div className='App'>
-      {/* <DocTitleOne></DocTitleOne> */}
-      {/* <DocTitleTwo></DocTitleTwo> */}
-      {/* <CounterOne></CounterOne> */}
-      {/* <CounterTwo></CounterTwo> */}
-      {/* <UserForm></UserForm> */}
-      <UserFormHook></UserFormHook>
+      <TransitionComp></TransitionComp>
     </div>
   );
 }
